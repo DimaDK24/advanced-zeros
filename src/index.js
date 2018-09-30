ae20ff7852154d371163963a8548aca6ef28f815
DimaDK <DimaKrutDK@ya.ru>
2018-09-30 20:25:08 +0300

    75/100 passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let count = 0;
+  const simpleMultipliers = getSimpleMultipliers(base);
+  const biggestMultiplier = Math.max(...simpleMultipliers);
+  for (let i = biggestMultiplier; number >= i; i = i * biggestMultiplier)
+    count += Math.floor(number / i);
+  return count;
+};
+
+function getSimpleMultipliers(number) {
+  let multipliers = [];
+  while (number % 2 === 0) {
+    multipliers.push(2);
+    number = Math.floor(number / 2);
+  }
+  for (let i = 3; i * i <= number; i += 2) {
+    if (number % i === 0) {
+      multipliers.push(i);
+      number = Math.floor(number / i);
+    }
+  }
+  if (number !== 1) multipliers.push(number);
+  return [...new Set(multipliers)];
 }
